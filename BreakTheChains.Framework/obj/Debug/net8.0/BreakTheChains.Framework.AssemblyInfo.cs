be48f0068db8f2b25fc748d5740fbb7aae3af5e0
Lexi <alexjw2110@gmail.com>
2024-12-27 03:00:45 +0000

    initial commit

--- BreakTheChains.Framework/obj/Debug/net8.0/BreakTheChains.Framework.AssemblyInfo.cs
+++ BreakTheChains.Framework/obj/Debug/net8.0/BreakTheChains.Framework.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BreakTheChains.Framework")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+349e9449fe90920d144b5f8797bf56cc1c09370c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BreakTheChains.Framework")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BreakTheChains.Framework")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
